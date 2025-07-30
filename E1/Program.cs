78d67e787615ec71a8e071f9f1a7a004d7dfe4fa
Diego Lopez <diego131322@gmail.com>
2025-07-29 20:30:26 -0600

    first excersice finished

---
+++ E1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingresa un número entero");
+int entero = Convert.ToInt32(Console.ReadLine());
+if (entero>0)
+{
+    for (int i = 1; i < 11; i++)
+    {
+        int resultado = entero * i;
+        Console.WriteLine(entero +" * " +i +" = " +resultado );
+    }
+}
+
